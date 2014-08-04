ac177ffde1bab811059b31b01daee1dcbbf3cb94
Erik Shandrowski <erik843@gmail.com>
2014-08-04 11:55:22 -0600

    Done except for bonus

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).on('ready', function() {
+  
+	var productsTemplateSource = $('#products-template').html();
+	var productsTemplate = Handlebars.compile(productsTemplateSource);
+
+	for (var i = 0; i < productsData.productsList.length; i++) {
+		$('#product-container')
+		.append( productsTemplate( productsData.productsList[i]))
+	};
+
+	var wishlistTemplateSource = $('#wishlist-item').html();
+	var wishlistTemplate = Handlebars.compile( wishlistTemplateSource);
+
+	$(document).on('click', '.add-wishlist', function(error) {
+		var gameTitle = $(this).siblings('.game-title').text();
+		console.log(gameTitle);
+		var gameCost = $(this).siblings('.game-cost').text();
+		console.log(gameCost);
+
+		$('#wishlist').append(wishlistTemplate( {title: gameTitle, cost: gameCost} ));
+	});
+
+	$(document).on('click', '.remove-button', function() {
+		$(this).closest('li').remove();
+	})
+
+});
